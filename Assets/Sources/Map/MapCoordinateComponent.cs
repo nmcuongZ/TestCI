a127cdb0ef0578869f306d0f898ee9ea4e4ce372
nmcuong <nmcuong6914@gmail.com>
2017-05-08 16:13:27 +0700

    First

---
+++ Assets/Sources/Map/MapCoordinateComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entitas;
+using Entitas.CodeGeneration.Attributes;
+
+[Game, Unique]
+
+public class MapCoordinateComponent : IComponent {
+    List<BlockPosition> blockList;
+}
+
+public class BlockPosition
+{
+    public int X, Y;
+    public Vector3 position;
+}
