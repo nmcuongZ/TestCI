f6c19e7d3060ed70ce36c0390dfba0d00ac30a41
nmcuong <nmcuong6914@gmail.com>
2017-05-09 22:30:55 +0600

    gff

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 - curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
 - curl -o Unity-iOS.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.4.2f2.pkg
 - sudo installer -dumplog -package Unity.pkg -target /
