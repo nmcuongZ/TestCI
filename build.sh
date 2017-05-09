65f5ed37be80a888d568b14fe10563f169f362bd
nmcuong <nmcuong6914@gmail.com>
2017-05-09 23:23:56 +0600

    nvb

--- build.sh
+++ build.sh
@@ -2,21 +2,17 @@
 
 project="Simple Mobile Placeholder"
 
-echo "Attempting to build $project for iOS"
+echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
+  -nographics \
   -silent-crashes \
-  -username "nmcuong22@yahoo.com.vn" \
-  -password "Helloworld123" \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -executeMethod CommandLineBuild.iOSBuild \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 echo 'Logs from build'
-cat $(pwd)/unity.log 
-
-cd Builds/iOS
-xcodebuild -project Unity-iPhone.xcodeproj -scheme Unity-iPhone clean build CONFIGURATION_BUILD_DIR=$CIRCLE_ARTIFACTS
+cat $(pwd)/unity.log
 
 
